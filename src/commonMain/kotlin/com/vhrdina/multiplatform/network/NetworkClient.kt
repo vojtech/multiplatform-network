c916e9b2a1a9c942ee81aa13916e12195fb914a1
Vojtech Hrdina <vhrdina@gmail.com>
2019-05-14 08:57:22 +0100

    Change lib version

--- src/commonMain/kotlin/com/vhrdina/multiplatform/network/NetworkClient.kt
+++ src/commonMain/kotlin/com/vhrdina/multiplatform/network/NetworkClient.kt
@@ -1,9 +1,6 @@
 package com.vhrdina.multiplatform.network
 
-import com.vhrdina.multiplatform.network.model.Config
-import com.vhrdina.multiplatform.network.model.NetworkError
-import com.vhrdina.multiplatform.network.model.Request
-import com.vhrdina.multiplatform.network.model.Response
+import com.vhrdina.multiplatform.network.model.*
 import com.vhrdina.multiplatform.network.util.HttpClientProvider
 import com.vhrdina.multiplatform.network.util.NetworkSerializer
 import com.vhrdina.multiplatform.network.util.RequestExecutor
@@ -16,6 +13,7 @@ class NetworkClient(val config: Config) {
         NetworkSerializer.apply {
             setMapper(NetworkError::class, NetworkError.serializer())
             setMapper(Config::class, Config.serializer())
+            setMapper(RequestConfig::class, RequestConfig.serializer())
         }
     }
 
